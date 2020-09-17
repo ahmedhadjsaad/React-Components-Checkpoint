9eec94643af5431c154f76c97c1c7c81b5d7f582
unknown <ahmed.hadjsaad@iit.ens.tn>
2020-09-17 11:22:45 +0200

    Updates

---
+++ precache-manifest.a6d67643d24ce0a5be5e505fd352a291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f549cd69bccb1eda4c5579e99b5ac3",
+    "url": "/React-Components-Checkpoint.gitgit/index.html"
+  },
+  {
+    "revision": "47919554be76ccf9cfe4",
+    "url": "/React-Components-Checkpoint.gitgit/static/css/main.9c278904.chunk.css"
+  },
+  {
+    "revision": "08d1333bd024455a89a0",
+    "url": "/React-Components-Checkpoint.gitgit/static/js/2.3fe57f69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Components-Checkpoint.gitgit/static/js/2.3fe57f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47919554be76ccf9cfe4",
+    "url": "/React-Components-Checkpoint.gitgit/static/js/main.0ee573b6.chunk.js"
+  },
+  {
+    "revision": "07afdc7509a245bd2106",
+    "url": "/React-Components-Checkpoint.gitgit/static/js/runtime-main.d514278b.js"
+  }
+]);
